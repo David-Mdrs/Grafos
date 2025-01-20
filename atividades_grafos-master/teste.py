36e532b8de8aa2d0972539bb809d81094c3b4c6a
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-01-20 19:10:50 -0300

    Criando matriz direcionada para teste

--- atividades_grafos-master/teste.py
+++ atividades_grafos-master/teste.py
@@ -1,25 +1,19 @@
-from meu_grafo_matriz_adj_nao_dir import MeuGrafo
+from meu_grafo_matriz_adj_dir import MeuGrafo
 
 # Grafo completo
 grafo = MeuGrafo()
-grafo.adiciona_vertice("E")
 grafo.adiciona_vertice("A")
-grafo.adiciona_vertice("F")
 grafo.adiciona_vertice("B")
 grafo.adiciona_vertice("C")
 grafo.adiciona_vertice("D")
-grafo.adiciona_vertice("G")
+grafo.adiciona_vertice("E")
 
-grafo.adiciona_aresta('a1', 'E', 'A')
-grafo.adiciona_aresta('a2', 'A', 'F')
-grafo.adiciona_aresta('a3', 'F', 'A')
-grafo.adiciona_aresta('a4', 'B', 'A')
-grafo.adiciona_aresta('a5', 'B', 'A')
-grafo.adiciona_aresta('a6', 'C', 'A')
-grafo.adiciona_aresta('a7', 'A', 'D')
-grafo.adiciona_aresta('a8', 'D', 'C')
-grafo.adiciona_aresta('a9', 'G', 'D')
+grafo.adiciona_aresta('a1', 'A', 'B')
+grafo.adiciona_aresta('a2', 'A', 'C')
+grafo.adiciona_aresta('a3', 'C', 'E')
+grafo.adiciona_aresta('a4', 'E', 'A')
+grafo.adiciona_aresta('a5', 'D', 'A')
+grafo.adiciona_aresta('a6', 'D', 'E')
 
 print("Imprimindo grafo: ")
 print(grafo)
-print(grafo.arestas_sobre_vertice('A'))
